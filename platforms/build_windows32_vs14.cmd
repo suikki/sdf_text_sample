dd1c638fc8764b20c930538b1480fa96a167b099
Olli Kallioinen <olli.kallioinen@iki.fi>
2018-01-04 17:39:13 +0200

    Build scripts cleaned up.

--- platforms/build_windows32_vs14.cmd
+++ platforms/build_windows32_vs14.cmd
@@ -2,17 +2,16 @@
 setlocal
 set PROJECT_DIR=%~dp0..
 set BUILD_DIR=%PROJECT_DIR%\build\windows32_vs14
-set CMAKE_EXE=cmake.exe
 
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%" || goto error
 pushd "%BUILD_DIR%" || goto error
 
 :: Configure cmake.
-call %CMAKE_EXE% -G "Visual Studio 14 2015" "%PROJECT_DIR%" || goto error
+call cmake -G "Visual Studio 14 2015" "%PROJECT_DIR%" || goto error
 
 :: Build the application.
-call %CMAKE_EXE% --build . --config Debug || goto error
-call %CMAKE_EXE% --build . --config Release || goto error
+call cmake --build . --config Debug || goto error
+call cmake --build . --config Release || goto error
 
 popd
 goto end
