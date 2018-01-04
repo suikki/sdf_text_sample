43282a05dae876b2d39858a6e717976be2dca9e1
Olli Kallioinen <olli.kallioinen@iki.fi>
2018-01-04 18:04:17 +0200

    Exit status should be unnecessary

--- platforms/build.sh
+++ platforms/build.sh
@@ -8,19 +8,19 @@
 #   >make
 #
 
-current_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P) || exit 1
+current_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P) || exit
 source_dir="${current_dir}/.."
 build_dir="${current_dir}/../build/make"
 
 
 # Build debug version
 cmake_options_debug="-DCMAKE_BUILD_TYPE:STRING=Debug"
-cmake -E make_directory "${build_dir}_debug" || exit 1
-cmake -E chdir "${build_dir}_debug" cmake $cmake_options_debug "$source_dir" || exit 1
-cmake -E chdir "${build_dir}_debug" cmake --build . || exit 1
+cmake -E make_directory "${build_dir}_debug" || exit
+cmake -E chdir "${build_dir}_debug" cmake $cmake_options_debug "$source_dir" || exit
+cmake -E chdir "${build_dir}_debug" cmake --build . || exit
 
 # Build Release version
 cmake_options="-DCMAKE_BUILD_TYPE:STRING=Release"
-cmake -E make_directory "${build_dir}" || exit 1
-cmake -E chdir "${build_dir}" cmake $cmake_options "$source_dir" || exit 1
-cmake -E chdir "${build_dir}" cmake --build . || exit 1
+cmake -E make_directory "${build_dir}" || exit
+cmake -E chdir "${build_dir}" cmake $cmake_options "$source_dir" || exit
+cmake -E chdir "${build_dir}" cmake --build . || exit
