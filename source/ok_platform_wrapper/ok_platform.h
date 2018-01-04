1be5fb94a48cc16fddc93aa1dc6261590f38d492
Olli Kallioinen <olli.kallioinen@iki.fi>
2018-01-04 23:33:30 +0200

    Minor formatting change

--- source/ok_platform_wrapper/ok_platform.h
+++ source/ok_platform_wrapper/ok_platform.h
@@ -119,11 +119,11 @@ freely, subject to the following restrictions:
 #endif
 
 
-
-
 #if defined(OK_PLATFORM_WINDOWS)
 #define WIN32_LEAN_AND_MEAN
+
 #include <windows.h>
+
 #endif
 
 #if defined(OK_PLATFORM_EMSCRIPTEN)
